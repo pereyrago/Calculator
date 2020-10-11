e80914128ed0390600e49e3e45d8b81fdcdbfa6c
Gabriel Pereyra <pereyrago@gmail.com>
2020-10-11 13:45:16 -0300

    Updates

---
+++ precache-manifest.a7457033edc81a04723ff8f229933549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e06d221c8f251a2278c9d556b11fcd9",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "6ab87f7f49e3376fea6d",
+    "url": "/Calculator/static/css/main.a59c6a3d.chunk.css"
+  },
+  {
+    "revision": "66e4f6f3e9a372266ed9",
+    "url": "/Calculator/static/js/2.c91cde77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.c91cde77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab87f7f49e3376fea6d",
+    "url": "/Calculator/static/js/main.d2f507ec.chunk.js"
+  },
+  {
+    "revision": "415d91827174b733efb5",
+    "url": "/Calculator/static/js/runtime-main.962f5acf.js"
+  },
+  {
+    "revision": "1467e2d37ec60cc50117a8db7c24f626",
+    "url": "/Calculator/static/media/fuente.1467e2d3.TTF"
+  }
+]);
